d04d037ec37bcfe5e40e8826771fd71f33d76090
Andy-Roger <andyrogerkats@gmail.com>
2019-05-19 08:33:53 -0500

    Added cubic bezier curves

---
+++ splineManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splineManager : MonoBehaviour
+{
+    //Instantiate segment into spline parent
+    [SerializeField] private Transform splineParent;
+    [SerializeField] private GameObject segment;
+    private List<splineSegment> segments = new List<splineSegment>();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            createSegment(splineParent);
+        }
+
+        //Update next point when node edited
+        updateSegmentPoints();
+    }
+
+    public void createSegment(Transform splineParent) {
+        var segment = Instantiate(this.segment, splineParent);
+        segments.Add(segment.GetComponent<splineSegment>());
+        //if first segment place at spline initial position
+        if (segment.transform.GetSiblingIndex() == 0)
+            segment.transform.localPosition = Vector3.zero;
+        else {
+            //if not first segment place segment at last segments waypoint_1 (point3)
+            var prevSegmentIndex = segment.transform.GetSiblingIndex() - 1;
+            var prevSegment = splineParent.GetChild(prevSegmentIndex).GetComponent<splineSegment>();
+            segment.transform.position = prevSegment.point3.position;
+        }
+    }
+
+    //On waypoint1 move (TODO), if there is a next segment, move the next segment's waypoint0 with this waypoint1
+    void updateSegmentPoints() {
+        if(segments.Count > 0) {
+            for (int i = 0; i < segments.Count - 1; i++) {
+                segments[i + 1].point0.transform.position = segments[i].point3.transform.position;
+            }
+        }
+    }
+}
---
+++ splineSegment.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splineSegment : MonoBehaviour {
+    [SerializeField] private LineRenderer splineLine;
+    [SerializeField] private LineRenderer controlPointLine_0;
+    [SerializeField] private LineRenderer controlPointLine_1;
+
+    public Transform point0, point1, point2, point3;
+    public int numPoints = 50;
+
+    private Vector3[] positions = new Vector3[50];
+
+    void Start() {
+        splineLine.positionCount = numPoints;
+        drawCubicCurve();
+    }
+
+    void Update() {
+        //Todo: only do drawCubicCurve if currently being edited
+        drawCubicCurve();
+        updateControlPointLine();
+    }
+
+    void drawCubicCurve() {
+        for (int i = 1; i < numPoints + 1; i++) {
+            float t = i / (float)numPoints;
+            positions[i - 1] = calculateCubicBezierPoint(t, point0.position, point1.position, point2.position, point3.position);
+        }
+        splineLine.SetPositions(positions);
+    }
+
+    private void updateControlPointLine() {
+        //Translate control point line ending to worldspace and set to cooresponding waypoint
+        controlPointLine_0.SetPosition(1, controlPointLine_0.transform.InverseTransformPoint(point0.position));
+        controlPointLine_1.SetPosition(1, controlPointLine_1.transform.InverseTransformPoint(point3.position));
+    }
+
+    private Vector3 calculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+        return p;
+    }
+}
+
